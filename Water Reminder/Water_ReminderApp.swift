f3572101f1e638a85119125cf95f756a51a71b2a
4KENSENS <kerem1eren@gmail.com>
2025-05-01 23:31:06 +0300

    Initial Commit

---
+++ Water Reminder/Water_ReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Water_ReminderApp.swift
+//  Water Reminder
+//
+//  Created by KEREM BOYLU on 1.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct Water_ReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
